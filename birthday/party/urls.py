238898e860f51a1cc50e889f388f505d394132da
okto <okto0914@gmail.com>
2024-09-03 17:26:13 +0900

    make login & room css & change carousel picture's width

--- birthday/party/urls.py
+++ birthday/party/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name ='main'),
     path('login/', views.login_view, name='login'),
     path('room/', views.room_view, name = 'room')
     
